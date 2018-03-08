daec35183f5dd9c51ff7ec08ea5535efa26929d5
kadaliphani <kadali.phanikishore21@gmail.com>
2018-03-08 16:59:05 +0530

    login.java

---
+++ login.java
@@ -0,0 +1,26 @@
+mport java.util.Scanner;
+public class BATM {
+
+public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+
+    String username;
+    String password;
+
+    System.out.println("Log in:");
+    System.out.println("Indtast username: ");
+    username = input.nextLine();
+
+    System.out.println("Indtast password: ");
+    password = input.nextLine();
+
+    users check = new users(username, password);
+
+    if(check.auth()) 
+        System.out.println("You are logged in");
+
+
+
+}
+
+}
